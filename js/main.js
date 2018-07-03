cb01e6d24f0cb0a29c2fde40d755bdbe19aa090e
Ravi Upadhyay <upadhyay-r@HCL.COM>
2018-07-03 12:22:22 +0530

    Stable code base

---
+++ js/main.js
@@ -0,0 +1,114 @@
+$(document).ready(function(){
+    console.log('document is ready');
+    getUsers();
+});
+
+const buttonLoadMore= document.getElementById('load-more-button');
+let lastResponse    = {};
+let users           = [];
+
+/**
+ * FUNCTION: This will get users from server. Here we are using `fetch()` method of 
+ * global object Window. It is replacement of XHR being used earlier. It will return
+ * promise that will again return `response` object.
+ */
+const getUsers = function(page = 1){
+    // console.log('getUsers has been clicked');
+    buttonLoadMore.setAttribute('disabled', true);
+    const baseApi   = 'https://reqres.in/api/users';
+    const finalApi  = baseApi + '?page=' + page;
+    const myHeaders = new Headers();
+    const myInit    = { 
+        method      : 'GET',
+        headers     : myHeaders,
+        mode        : 'cors',
+        cache       : 'default' };
+
+    const myRequest = new Request(finalApi, myInit);
+
+    fetch(myRequest).then(function(response) {
+        console.log('getUsers successful');
+        response.json().then(function(responseData){
+            lastResponse  =   responseData;
+            users = [...users, ...responseData.data];
+            console.log(users);
+            if(responseData.page < responseData.total_pages){
+                buttonLoadMore.removeAttribute('disabled');
+            }
+            updateView(responseData.data);
+            // (function(){
+            //     let dataArray = responseData.data;
+            //     const displayElement = document.getElementById('users-display-wrapper');
+            //     // if(dataArray.length){
+            //     for (let i = 0; i <= dataArray.length; i++){
+            //         const dataObject = dataArray[i];
+            //         console.log(dataObject);
+            //         const newCard = [
+            //         '<div class="col-xs-12 col-md-4 mx-auto" style="margin-bottom: 20px;">',
+            //         '<div class="card" id="',
+            //         dataObject.id,
+            //         '">',
+            //         '<img class="card-img-top" src="',
+            //         dataObject.avatar,
+            //         '" alt="Avatar">',
+            //         '<div class="card-body">',
+            //         '<p class="card-text">',
+            //         '<h5>First Name = ',
+            //         dataObject.first_name,
+            //         '</h5>',
+            //         '<h5>Last Name = ',
+            //         dataObject.last_name,
+            //         '</h5>',
+            //         '</p>',
+            //         '</div>',
+            //         '</div>',
+            //         '</div>',
+            //         ];
+            
+            //         displayElement.innerHTML += newCard.join('');
+            //     }
+            // // }
+            // })();
+        });
+    }, function(){
+        console.log('getUsers unsuccessful');
+        window.location = '../404.html';
+    });
+};
+
+const loadMore = function(){
+    if(lastResponse.page < lastResponse.total_pages){
+        getUsers(lastResponse.page+1);
+    }
+}
+
+const updateView = function(dataArray){
+    const displayElement = document.getElementById('users-display-wrapper');
+    for (let i = 0; i < dataArray.length; i++){
+        var dataObject = dataArray[i];
+        console.log(dataObject);
+        const newCard = [
+        '<div class="col-xs-12 col-md-4 mx-auto" style="margin-bottom: 20px;">',
+        '<div class="card" id="',
+        dataObject.id,
+        '">',
+        '<img class="card-img-top" src="',
+        dataObject.avatar,
+        '" alt="Avatar">',
+        '<div class="card-body">',
+        '<p class="card-text">',
+        '<h5>First Name = ',
+        dataObject.first_name,
+        '</h5>',
+        '<h5>Last Name = ',
+        dataObject.last_name,
+        '</h5>',
+        '</p>',
+        '</div>',
+        '</div>',
+        '</div>',
+        ];
+
+        displayElement.innerHTML += newCard.join('');
+    }
+};
